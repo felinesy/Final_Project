ee53911657e01980a2e59ac795e98d062ecc0e8d
Katrina Rubi <rubikatrinaa@gmail.com>
2026-04-29 13:20:39 +0800

    Add game loop and thread handling

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -94,3 +94,29 @@ public class GamePanel extends JPanel implements Runnable{
         specialBookKeyMap.put("Sbook3", new Obj_SpecialKey("Skey3", this));
     }
 
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime-lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+
+    }
+
